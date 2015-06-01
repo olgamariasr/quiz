840d9e001a9ddeb5a0910c11d869afcf8b6ea26b
olga <olga.maria.s.r@gmail.com>
2015-06-01 19:29:34 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+// b: Importar quiz_controller.js en routes/index.js.js
+var quizController = require('../controllers/quiz_controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+// Introducir nuevas rutas en el enrutador routes/index.js
+// GET /quizes/question y GET /quizes/answer
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;

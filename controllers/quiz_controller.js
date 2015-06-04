3e4af7f8cbe57cb645103f624c0d1f9e7abb6641
olga <olga.maria.s.r@gmail.com>
2015-06-04 14:32:28 +0200

    Previo a bbdd heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,7 @@
  	res.render('autor'); 	
  };
  // GET /quizes/question
-
+/*
  exports.question = function(req, res){
 
  	res.render('quizes/question',{pregunta:'Capital de Italia'}); 	
@@ -16,3 +16,22 @@
  		res.render('quizes/answer',{respuesta:'Incorrecto'});
  	}
  }; 
+ */
+var models= require('../models/models.js');
+  // GET /quizes/question
+ exports.question = function(req, res){
+ 	models.Quiz.findAll().success(function (quiz) {
+ 	res.render('quizes/question',{pregunta:quiz[0].pregunta}); 	
+ })
+ };
+
+ exports.answer = function(req, res){
+ 	models.Quiz.findAll().success(function (quiz) {
+
+ 	if (req.query.respuesta== quiz[0].respuesta){
+ 		res.render('quizes/answer',{respuesta:'Correcto'});		
+ 	}else{
+ 		res.render('quizes/answer',{respuesta:'Incorrecto'});
+ 	}
+ 	 })
+ };  
